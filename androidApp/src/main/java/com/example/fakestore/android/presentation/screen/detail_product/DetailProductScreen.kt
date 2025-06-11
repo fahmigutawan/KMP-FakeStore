40bb2caa25dd4fb8d394ded3ac396f1c0ef34824
Fahmi Noordin Rumagutawan <fahmigutawan@gmail.com>
2025-06-11 16:00:28 +0700

    chore: finishd simpler version of detail_product

--- androidApp/src/main/java/com/example/fakestore/android/presentation/screen/detail_product/DetailProductScreen.kt
+++ androidApp/src/main/java/com/example/fakestore/android/presentation/screen/detail_product/DetailProductScreen.kt
@@ -1,10 +1,34 @@
 package com.example.fakestore.android.presentation.screen.detail_product
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil3.compose.AsyncImage
+import com.example.fakestore.util.Resource
 
 @Composable
 fun DetailProductScreen(id: Int) {
@@ -13,5 +37,74 @@ fun DetailProductScreen(id: Int) {
 
     LaunchedEffect(true) {
         viewModel.getProductById(id)
+    }
+
+    Scaffold {
+        when (product.value) {
+            is Resource.Error<*> -> {}
+            is Resource.Loading<*> -> {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(it),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(32.dp),
+                        strokeWidth = 4.dp
+                    )
+                }
+            }
+
+            is Resource.Success<*> -> {
+                Column(
+                    modifier = Modifier
+                        .padding(it)
+                        .fillMaxSize()
+                        .verticalScroll(rememberScrollState())
+                        .background(MaterialTheme.colorScheme.surface),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .aspectRatio(4f / 3f)
+                            .background(Color.White),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        AsyncImage(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(24.dp),
+                            model = product.value.data?.image,
+                            contentDescription = "",
+                            contentScale = ContentScale.Fit
+                        )
+                    }
+
+                    Divider(
+                        modifier = Modifier.fillMaxWidth(),
+                        thickness = 2.dp
+                    )
+
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(16.dp)
+                    ) {
+                        Text(
+                            product.value.data?.title ?: "-",
+                            style = MaterialTheme.typography.headlineMedium,
+                            fontWeight = FontWeight.SemiBold,
+                            modifier = Modifier.padding(horizontal = 24.dp)
+                        )
+
+                        Text(
+                            product.value.data?.description ?: "-",
+                            modifier = Modifier.padding(horizontal = 24.dp)
+                        )
+                    }
+
+                }
+            }
+        }
     }
 }
